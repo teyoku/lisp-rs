e77fb614fbfd67d2dadae44e4fa64cab1891f07b
teyoku <teyoku.dev@gmail.com>
2026-08-15 22:29:04 +0500

    add LexerError enum for error handling in the lexer

---
+++ src/errors.rs
@@ -0,0 +1,19 @@
+use std::error::Error;
+
+#[derive(Debug)]
+pub enum LexerError {
+    UnknownSymbol(char),
+    UnterminatedString(String),
+}
+
+impl std::fmt::Display for LexerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let error_msg = match self {
+            LexerError::UnknownSymbol(ch) => &format!("Unknown symbol '{ch}'"),
+            LexerError::UnterminatedString(s) => &format!("Unterminated string {s}"),
+        };
+
+        write!(f, "[Lexer Error] {error_msg}")
+    }
+}
+impl Error for LexerError {}
