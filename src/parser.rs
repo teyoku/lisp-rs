9bf643c689fa41f13bb42362ce328ea93d9673d6
teyoku <teyoku.dev@gmail.com>
2026-08-16 21:37:40 +0500

    feat: implement Parser struct with parsing logic for nested expressions

---
+++ src/parser.rs
@@ -0,0 +1,107 @@
+use crate::{errors::ParseError, lexer::Token, object::Object};
+
+pub struct Parser {
+    tokens: Vec<Token>,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self {
+            tokens: tokens.into_iter().rev().collect(),
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Object, ParseError> {
+        let token = self.tokens.pop();
+        if token != Some(Token::LParen) {
+            return Err(ParseError {
+                err: format!("Expected '(' found '{:?}'", token),
+            });
+        }
+
+        let mut list = Vec::new();
+        while let Some(token) = self.tokens.pop() {
+            match token {
+                Token::Keyword(k) => list.push(Object::Keyword(k)),
+                Token::BinaryOp(b) => list.push(Object::BinaryOp(b)),
+                Token::Integer(n) => list.push(Object::Integer(n)),
+                Token::Float(n) => list.push(Object::Float(n)),
+                Token::String(s) => list.push(Object::String(s)),
+                Token::Symbol(s) => list.push(Object::Symbol(s)),
+                Token::LParen => {
+                    self.tokens.push(Token::LParen);
+                    let sub_list = self.parse()?;
+                    list.push(sub_list);
+                }
+                Token::RParen => return Ok(Object::List(list)),
+            }
+        }
+
+        Ok(Object::List(list))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::Lexer;
+
+    use super::*;
+
+    fn create_parser(program: &str) -> Parser {
+        let tokens = Lexer::new(program).tokenize();
+        Parser::new(tokens)
+    }
+
+    #[test]
+    fn test_add() -> Result<(), ParseError> {
+        let list = create_parser("(+ 1 2)").parse()?;
+        assert_eq!(
+            list,
+            Object::List(vec![
+                Object::BinaryOp("+".to_string()),
+                Object::Integer(1),
+                Object::Integer(2)
+            ])
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_area_of_a_circle() -> Result<(), ParseError> {
+        let program = "
+            (
+                (define r 10)
+                (define pi 314)
+                (* pi (* r r))
+            )
+        ";
+        let list = create_parser(program).parse()?;
+        assert_eq!(
+            list,
+            Object::List(vec![
+                Object::List(vec![
+                    Object::Keyword("define".to_string()),
+                    Object::Symbol("r".to_string()),
+                    Object::Integer(10)
+                ]),
+                Object::List(vec![
+                    Object::Keyword("define".to_string()),
+                    Object::Symbol("pi".to_string()),
+                    Object::Integer(314)
+                ]),
+                Object::List(vec![
+                    Object::BinaryOp("*".to_string()),
+                    Object::Symbol("pi".to_string()),
+                    Object::List(vec![
+                        Object::BinaryOp("*".to_string()),
+                        Object::Symbol("r".to_string()),
+                        Object::Symbol("r".to_string()),
+                    ])
+                ]),
+            ])
+        );
+
+        Ok(())
+    }
+}
