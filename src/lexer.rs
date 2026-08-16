c82d3094188c4fa935240748684ecf941b40d1ef
teyoku <teyoku.dev@gmail.com>
2026-08-16 20:05:11 +0500

    feat: implement Lexer struct for tokenizing input

---
+++ src/lexer.rs
@@ -0,0 +1,18 @@
+use std::str::Chars;
+
+pub struct Lexer<'a> {
+    input: Chars<'a>,
+    current_char: Option<char>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        let mut chars = input.chars();
+        let current_char = chars.next();
+
+        Self {
+            input: chars,
+            current_char,
+        }
+    }
+}
