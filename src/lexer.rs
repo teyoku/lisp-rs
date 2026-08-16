d5fa4e8128143fd4b2353286b549c46b1468702c
teyoku <teyoku.dev@gmail.com>
2026-08-16 20:46:07 +0500

    feat: enhance Lexer with Token enum and keyword/binary operator sets

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,8 +1,21 @@
-use std::str::Chars;
+use std::{collections::HashSet, str::Chars};
+
+pub enum Token {
+    Integer(i64),
+    Symbol(String),
+    LParen,
+    RParen,
+    Float(f64),
+    String(String),
+    BinaryOp(String),
+    Keyword(String),
+}
 
 pub struct Lexer<'a> {
     input: Chars<'a>,
     current_char: Option<char>,
+    keywords: HashSet<&'a str>,
+    binary_ops: HashSet<char>,
 }
 
 impl<'a> Lexer<'a> {
@@ -10,9 +23,29 @@ impl<'a> Lexer<'a> {
         let mut chars = input.chars();
         let current_char = chars.next();
 
+        let keywords: HashSet<&str> =
+            vec!["define", "print", "lambda", "if", "cond", "else", "let"]
+                .into_iter()
+                .collect();
+
+        let binary_ops: HashSet<char> = vec!['+', '-', '*', '/', '%', '<', '>', '=', '&', '|']
+            .into_iter()
+            .collect();
+
         Self {
             input: chars,
             current_char,
+            keywords,
+            binary_ops,
         }
+    }
+
+    fn next_token(&mut self) -> Option<Token> {
+        todo!()
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        self.current_char = self.input.next();
+        self.current_char
     }
 }
